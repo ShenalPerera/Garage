583b08857f4f84e64a6f7e4910c7f7a5c58bbcd7
Shenal Perera <shenalperera00@gmail.com>
2024-02-07 10:03:12 +0530

    [Backend] - 2 : implemented the basic login

--- Backend/garage-service/src/main/java/org/isa/garage/controller/GarageRestController.java
+++ Backend/garage-service/src/main/java/org/isa/garage/controller/GarageRestController.java
@@ -1,16 +1,17 @@
 package org.isa.garage.controller;
 
 import jakarta.validation.Valid;
-import org.isa.garage.dao.UserDao;
+import org.isa.garage.dto.UserLoginDTO;
 import org.isa.garage.dto.UserSignupDTO;
 import org.isa.garage.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,8 @@ public class GarageRestController {
 
     private final UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     public GarageRestController(UserService userService){
         this.userService  =userService;
@@ -35,5 +38,17 @@ public class GarageRestController {
             return ResponseEntity.status(HttpStatus.CREATED).body("User created");
         else
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userLoginDTO){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userLoginDTO.getEmail(),userLoginDTO.getPassword())
+        );
+
+        if (authentication.isAuthenticated()){
+            return ResponseEntity.status(200).build();
+        }
+        return ResponseEntity.status(404).build();
     }
 }
